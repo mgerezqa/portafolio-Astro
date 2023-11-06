3a816823e134c6ebf897a1e3bdb07f69e3e24314
Melli11 <mingerez@gmail.com>
2023-11-06 00:00:09 -0300

    first commit

---
+++ src/utils/AppConfig.ts
@@ -0,0 +1,14 @@
+export const AppConfig = {
+  site_url: 'https://akhilgkrishnan.me',
+  site_name: 'Akhil G Krishnan | Full Stack Web Developer',
+  title: 'Akhil G Krishnan',
+  description:
+    "Explore Akhil G Krishnan's 4 years of full-stack development expertise, open source contributions, and passion for Ruby on Rails and React.",
+  keywords:
+    'akhil, akhil g, akhil krishnan, akhil g krishnan, portfolio, sealoun, activatecare, best web designer in kerala, india, web developer, ethical hacker, cyberSecurity researcher, rails developer, react developers, javascript',
+  author: 'Akhil G Krishnan',
+  locale_region: 'en-us',
+  locale: 'en',
+  ogImage: 'https://akhilgkrishnan.me/profile-pic.png',
+  twitter_handle: '@MrAkhilG',
+};
